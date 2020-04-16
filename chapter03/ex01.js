404bffe5540957013c1094fd8b2980bc4f71f990
Lucas-Severo <lucas_severo@outlook.com.br>
2020-04-16 19:26:05 -0400

    Add the first exercise from chapter three

---
+++ chapter03/ex01.js
@@ -0,0 +1,8 @@
+const min = function (number1, number2) {
+    return number1 <= number2 ? number1 : number2;
+}
+
+console.log(min(0, 10));
+console.log(min(0, -10));
+console.log(min(10, 10));
+console.log(min(7, 5));
